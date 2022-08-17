9b3afa7a52670df97aa5a282140bd27f1510507b
KrakenDev0001 <111214932+KrakenDev0001@users.noreply.github.com>
2022-08-17 16:18:26 -0500

    Fixed???

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,3 @@
 if tostring(game.PlaceId) == '9498006165' then
-    loadstring(game:HttpGet('https://scripts.luawl.com/MythHub/TappingSimulator.lua'))('KrakenDev better thank Chwome')
+    loadstring(game:HttpGet('https://scripts.luawl.com/MythHub/TappingSimulator.lua'))()
 end
