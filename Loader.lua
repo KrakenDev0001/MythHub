f1859bcff273b30932de703fba554675f652bf57
KrakenDev0001 <111214932+KrakenDev0001@users.noreply.github.com>
2022-08-17 22:00:59 -0500

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,3 +1,6 @@
 if tostring(game.PlaceId) == '9498006165' then
     loadstring(game:HttpGet('https://scripts.luawl.com/MythHub/TappingSimulator.lua'))()
 end
+if tostring(game.PlaceId) == '537413528' then
+    loadstring(game:HttpGet('https://scripts.luawl.com/MythHub/BaBfT.lua'))()
+end
