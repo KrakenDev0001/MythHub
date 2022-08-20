e09cd53d76b4f1bfe20afd33c60a3326c270b184
i have a lisp <66950396+ceasondz@users.noreply.github.com>
2022-08-20 03:07:16 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -7,3 +7,6 @@ end
 if tostring(game.PlaceId) == '7991339063' then
     loadstring(game:HttpGet('https://scripts.luawl.com/MythHub/RainbowFriends.lua'))()
 end
+if tostring(game.PlaceId) == '8540346411' then
+    loadstring(game:HttpGet('https://scripts.luawl.com/MythHub/RebirthChampionsX.lua'))()
+end
